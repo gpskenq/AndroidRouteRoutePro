76cf706223e22c68b2178e7587fe406a3490fb0e
gpskenqroute <gpskenq@gmail.com>
2019-03-18 17:02:00 +0800

    二回同じもの入力させて一致していない場合ボタン活性化しない

--- app/src/main/java/jp/co/nri/route/activity/SignUpActivity.java
+++ app/src/main/java/jp/co/nri/route/activity/SignUpActivity.java
@@ -1,6 +1,10 @@
 package jp.co.nri.route.activity;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,6 +16,7 @@ import jp.co.nri.route.base.BaseApplication;
 import jp.co.nri.route.di.component.DaggerSignUpComponent;
 import jp.co.nri.route.di.module.SignUpModule;
 import jp.co.nri.route.presenter.SignUpPresenter;
+import jp.co.nri.route.util.AppUtil;
 import jp.co.nri.route.view.ISignUpView;
 
 public class SignUpActivity extends BaseActivity<SignUpPresenter> implements ISignUpView {
@@ -21,6 +26,8 @@ public class SignUpActivity extends BaseActivity<SignUpPresenter> implements ISi
     @BindView(R.id.etName) EditText etName;
     @BindView(R.id.etUserID) EditText etUserID;
     @BindView(R.id.etPassword) EditText etPassword;
+    @BindView(R.id.btnSignUp) Button btnSignUp;
+    @BindView(R.id.etRepeatPassword) EditText etRepeatPassword;
 
     @Override
     protected int contentView() {
@@ -34,6 +41,13 @@ public class SignUpActivity extends BaseActivity<SignUpPresenter> implements ISi
     protected void initView() {
         tvToolbarBack.setVisibility(View.VISIBLE);
         tvToolbarTitle.setVisibility(View.VISIBLE);
+        btnSignUp.setEnabled(false);
+        MyTextWatcher myTextWatcher = new MyTextWatcher();
+        etName.addTextChangedListener(myTextWatcher);
+        etUserID.addTextChangedListener(myTextWatcher);
+        etPassword.addTextChangedListener(myTextWatcher);
+        etRepeatPassword.addTextChangedListener(myTextWatcher);
+
         setStatusBarColor(R.color.colorAccent, R.color.colorAccent, R.color.colorAccent);
         DaggerSignUpComponent.builder().appComponent(BaseApplication.getApplication().getAppComponent()).signUpModule(new SignUpModule(this)).build().inject(this);
     }
@@ -53,5 +67,32 @@ public class SignUpActivity extends BaseActivity<SignUpPresenter> implements ISi
     @Override
     public void closeView() {
         finish();
+    }
+
+
+    private class MyTextWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if(TextUtils.isEmpty(etName.getText().toString()) ||
+                    TextUtils.isEmpty(etUserID.getText().toString()) ||
+                    TextUtils.isEmpty(etPassword.getText().toString()) ||
+                    TextUtils.isEmpty(etRepeatPassword.getText().toString())){
+                btnSignUp.setEnabled(false);
+            }else{
+                btnSignUp.setEnabled((etPassword.getText().toString().equals(etRepeatPassword.getText().toString())));
+            }
+
+        }
     }
 }
